f91128ca60348bd5cd88f87ed627f3a72d13b4b1
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2015-09-25 21:54:54 +0300

    fix test

--- paging_test.go
+++ paging_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 var cases = map[Paging][]int{
-	Paging{Page: 0, Limit: 0, Count: 0}:   []int{10, 0, 1, 0},
+	Paging{Page: 0, Limit: 0, Count: 0}:   []int{10, 0, 1, 1},
 	Paging{Page: 1, Limit: 10, Count: 19}: []int{10, 0, 1, 2},
 	Paging{Page: 3, Limit: 5, Count: 248}: []int{5, 10, 3, 50},
 	Paging{Page: 2, Limit: 1, Count: 100}: []int{1, 1, 2, 100},
